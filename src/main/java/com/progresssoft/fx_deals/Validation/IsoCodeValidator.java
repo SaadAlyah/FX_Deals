af26993f8c216df9101208a70e8f2d8801492a8e
SaadAlyah <36413739+SaadAlyah@users.noreply.github.com>
2022-02-25 20:25:31 +0300

    Adding logs

--- src/main/java/com/progresssoft/fx_deals/Validation/IsoCodeValidator.java
+++ src/main/java/com/progresssoft/fx_deals/Validation/IsoCodeValidator.java
@@ -1,12 +1,17 @@
 package com.progresssoft.fx_deals.Validation;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.Currency;
 import java.util.Set;
 
 public class IsoCodeValidator implements ConstraintValidator<IsoCode, String> {
+
+    Logger logger = LoggerFactory.getLogger(IsoCodeValidator.class);
 
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
@@ -16,6 +21,7 @@ public class IsoCodeValidator implements ConstraintValidator<IsoCode, String> {
         try {
             containsIsoCode = currencies.contains(Currency.getInstance(s));
         }catch(IllegalArgumentException e){
+            logger.error("error occurred while validating currency Ex: ", e);
         }
         return containsIsoCode;
     }
